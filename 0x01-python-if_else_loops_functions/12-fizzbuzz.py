22372f0f5f6f2f4dd6ec54a287f29ac6be5a049c
fadikoubaa19 <2062@holbertonschool.com>
2020-09-03 21:29:09 +0000

    fffffffffffffffffff

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for c in range(1, 101):
-        if (c % 3 == 0 and i % c == 0):
+        if (c % 3 == 0 and c % c == 0):
             print("FizzBuzz ", end='')
         elif (c % 3 == 0):
             print("Fizz ", end='')
