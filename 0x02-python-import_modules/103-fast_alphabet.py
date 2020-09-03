dd95d6d3f8ea3726b2e2bf0f475a0fea9be23903
fadikoubaa19 <2062@holbertonschool.com>
2020-09-03 22:59:46 +0000

    eaeaea

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+from alpha import uppercaseAlphabets
+uppercaseAlphabets() 
---
+++ 0x02-python-import_modules/alpha.py
@@ -0,0 +1,5 @@
+def uppercaseAlphabets(): 
+  
+    # uppercase 
+    for c in range(65, 91): 
+        print(chr(c), end = "")
