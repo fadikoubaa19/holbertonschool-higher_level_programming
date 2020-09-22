691bc4e726a20745637970264b77bed481901b36
fadikoubaa19 <2062@holbertonschool.com>
2020-09-22 20:50:11 +0000

    the square size

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-class Square:
-    _Square__size = "Square Validation"
-    _Square__size = {}
+"""set the square's size
+"""
 
+
+class Square:
+    """set the square's size
+    """
     def __init__(self, size=0):
-        if type(size) == int:
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self._Square__size = size
-        else:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
