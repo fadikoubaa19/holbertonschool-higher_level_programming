72a60118f21e9adc5d53ac9a1b45bf77f2f56d17
fadikoubaa19 <2062@holbertonschool.com>
2020-09-22 20:40:23 +0000

    python is fun

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+class Square:
+    _Square__size = "Square Validation"
+    _Square__size = {}
+
+    def __init__(self, size=0):
+        if type(size) == int:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self._Square__size = size
+        else:
+            raise TypeError("size must be an integer")
