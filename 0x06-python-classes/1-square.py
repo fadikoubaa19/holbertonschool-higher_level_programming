dac9e2066c8754de4c79e13146a6de8184ffdd4c
fadikoubaa19 <2062@holbertonschool.com>
2020-09-22 20:51:23 +0000

    fixing

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
-"""class and size
-"""
-
-
 class Square:
-    """class and size
-    """
-    def __init__(self, __size):
-        self.__size = __size
+    def __init__(self, size):
+        self.__size = size
