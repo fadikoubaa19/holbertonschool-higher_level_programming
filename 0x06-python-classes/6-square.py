a168d5eca37622ab345a3394cde39735023da189
fadikoubaa19 <2062@holbertonschool.com>
2020-09-23 13:25:08 +0000

    fixed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,53 +1,72 @@
 #!/usr/bin/python3
-"""Square class
+"""empty
 """
 
 
 class Square:
-    """Square class
+    """square size
     """
-    def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+    def __init__(self, size=0, position=()):
+        if isinstance(size, int):
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if position:
+            error = "position must be a tuple of 2 positive integers"
+            if len(position) != 2:
+                raise TypeError(error)
+            if not isinstance(position[0], int) or position[0] < 0:
+                raise TypeError(error)
+            if not isinstance(position[1], int) or position[1] < 0:
+                raise TypeError(error)
+            self.__position = position
 
     def area(self):
-        return(self.__size ** 2)
+        return (self.__size ** 2)
 
     def my_print(self):
-        if not self.__size:
-            print("")
+        if self.__size == 0:
+            print()
         else:
-            for i in range(self.__position[1]):
-                print("")
-            for j in range(0, self.__size):
-                for x in range(self.__position[0]):
-                    print(" ", end='')
-                for y in range(self.__size):
-                    print("#", end='')
-                print("")
+            if hasattr(self, 'position'):
+                for k in range(self.__position[1]):
+                    print()
+            for i in range(self.__size):
+                if hasattr(self, 'position'):
+                    for l in range(self.__position[0]):
+                        print(" ", end="")
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
 
     @property
     def size(self):
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        return (self.__size)
 
     @property
     def position(self):
-        return self.__position
+        return (self.__position)
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
 
     @position.setter
     def position(self, value):
-        if len(value) != 2 or not isinstance(value, tuple) or\
-           not isinstance(value[0], int) or value[0] < 0 or\
-           not isinstance(value[1], int) or value[1] < 0:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
+        error = "position must be a tuple of 2 positive integers"
+        if len(value) != 2:
+            raise TypeError(error)
+        if not isinstance(value[0], int) or value[0] < 0:
+            raise TypeError(error)
+        if not isinstance(value[1], int) or value[1] < 0:
+            raise TypeError(error)
+        self.__position = value
