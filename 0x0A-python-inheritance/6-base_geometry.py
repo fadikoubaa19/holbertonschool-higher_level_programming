cfa21f87098459052c2e5c550c82070e3ecd0a20
fadikoubaa19 <2062@holbertonschool.com>
2020-10-06 16:25:07 +0000

    add exeception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""class BaseGeometry """
+
+
+class BaseGeometry:
+    """class BaseGeometry with exception """
+
+    def area(self):
+        """ add exception"""
+        raise Exception('area() is not implemented')
