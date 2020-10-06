846ab33498db9e18e4d4be8aca467640df618c98
fadikoubaa19 <2062@holbertonschool.com>
2020-10-06 16:22:06 +0000

    empty class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,5 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    """True if the object is an instance of a class that inherited directly or indirectly"""
+    """an instance of a class that inherited directly or indirectly"""
     return issubclass(type(obj), a_class) and type(obj) != a_class
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" empty BaseGemotry """
+
+
+class BaseGeometry:
+    """ BaseGeometry """
+    pass
