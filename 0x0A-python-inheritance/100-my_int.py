ae31a4663dd1ff00a8b28f31a29a4169e697b191
fadikoubaa19 <2062@holbertonschool.com>
2020-10-06 17:36:48 +0000

    fixed

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python3
-"""this module contain my_int"""
+"""
+This module contain my int
+"""
 
 
 class MyInt(int):
+    """
+    define class
+    """
     def __init__(self, value):
+        """
+        function init
+        """
         self.value = value
 
     def __eq__(self, obj):
+        """
+        overrides the equal int method
+        """
         return (obj != self.value)
 
     def __ne__(self, obj):
+        """
+        function not equal
+        """
         return (obj == self.value)
