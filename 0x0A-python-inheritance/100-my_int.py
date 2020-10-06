62aba1bfb5fa3ea22edd8ecd143f57527a833280
fadikoubaa19 <2062@holbertonschool.com>
2020-10-06 17:18:12 +0000

    fixed ()

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-""" modue that contains my int"""
-
-
 class MyInt(int):
-
-    def __init__(self, value):
-        """function init"""
-        self.value = value
+    """ defines class MyInt """
 
     def __eq__(self, other):
-        """function equal =!"""
-        return self.value != other
+        """ function faking """
+        return int.__ne__(self, other)
 
     def __ne__(self, other):
-        """function equal =="""
-        return self.value == other
+        """function faking """
+        return int.__eq__(self, other)
