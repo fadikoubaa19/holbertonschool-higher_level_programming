b6cba546e1d8695b4f6d757cb992406e732ced15
fadikoubaa19 <2062@holbertonschool.com>
2020-10-06 15:17:18 +0000

    issubclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" True if the object is an instance of a class t"""
+
+
+def inherits_from(obj, a_class):
+    """True if the object is an instance of a class that inherited directly or indirectly"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
