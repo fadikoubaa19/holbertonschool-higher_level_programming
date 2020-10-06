d967e763fb0ed7b6bd2d8afc1884b47899dfe01d
fadikoubaa19 <2062@holbertonschool.com>
2020-10-06 17:28:36 +0000

    fixed

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-"""
-This module add a new int subclass
-"""
+"""this module contain my_int"""
 
 
 class MyInt(int):
-    """
-    This is the int subclass
-    """
     def __init__(self, value):
-        """
-        This is the MyInt init method
-        """
         self.value = value
 
     def __eq__(self, obj):
-        """
-        This method overrides the equal int method
-        """
         return (obj != self.value)
 
     def __ne__(self, obj):
-        """
-        This method overrides the not equal int method
-        """
         return (obj == self.value)
