0132da171721cf5ab6e4e4a4254bd1fc6c5a589d
fadikoubaa19 <2062@gmail.com>
2020-11-17 14:04:43 +0000

    add comment in task 1

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- List all priveleges
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+-- list privelege
+-- i hate comments
+SHOW GRANTS FOR 'user_0d_1'@'localhost'; 
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
