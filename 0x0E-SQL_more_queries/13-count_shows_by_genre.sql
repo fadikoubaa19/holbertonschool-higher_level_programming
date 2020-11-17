478b437247185ca4cf1545f827583aaa08066fca
fadikoubaa19 <2062@gmail.com>
2020-11-17 21:28:08 +0000

    ok elttt

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,4 @@
--- i hate 2 comments
---lists all genres in hbetn
+-- 2 comm
+-- list all by geners
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id GROUP BY tv_show_genres.genre_id ORDER BY COUNT(tv_show_genres.genre_id)DESC;
 
