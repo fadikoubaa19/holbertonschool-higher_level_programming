b48eebbc6aa540e58393ae8426d7c3f46bf79498
fadikoubaa19 <2062@gmail.com>
2020-11-17 21:24:33 +0000

    add 2 commit

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,4 @@
 -- i hate 2 comments
 --lists all genres in hbetn
-SELECT tv_genres.name AS genre, COUNT(*) AS number_shows FROM tv_show_genres
-INNER JOIN  tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name ORDER BY number_shows DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id GROUP BY tv_show_genres.genre_id ORDER BY COUNT(tv_show_genres.genre_id)DESC;
+
