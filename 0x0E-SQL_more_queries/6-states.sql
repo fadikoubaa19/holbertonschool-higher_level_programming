218071e881c51b7b560b19386cfb62d602410d10
fadikoubaa19 <2062@gmail.com>
2020-11-17 14:17:51 +0000

    scirpt that create a data base

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- Script  that create a data base
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id int UNIQUE NOT NULL AUTO_INCREMENT, name varchar(256) NOT NULL, PRIMARY KEY (id));
+
