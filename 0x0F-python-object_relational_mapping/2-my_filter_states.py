3d7721e0d051096da39a7cdda2a3262406157461
fadikoubaa19 <fadi.koubaa@gmail.com>
2020-12-13 20:47:11 +0100

    3rd task

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+''' Module to print all states '''
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    woof = db.cursor()
+    woof.execute("SELECT * from states WHERE name LIKE '{}' \
+                COLLATE latin1_general_cs\
+                ORDER BY states.id".format(argv[4]))
+    for row in woof.fetchall():
+        print(row)
+    woof.close()
+    db.close()
