2e1a091b35feb88a6a7bcc4029e1443ddfb6f3a0
fadikoubaa19 <fadi.koubaa@gmail.com>
2020-12-13 22:24:31 +0100

    p

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """module contain model city"""
-
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import State
 
 Base = declarative_base()
 
 
-class State(Base):
-    """class state """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(128), nullable=False)
+class City(Base):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, unique=True,
+                nullable=False)
+    name = Column(String(128),
+                  nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
