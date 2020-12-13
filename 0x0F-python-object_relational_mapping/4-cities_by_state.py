9bc29765778643cff76dd4df8c1f40b8bdf33da0
fadikoubaa19 <fadi.koubaa@gmail.com>
2020-12-13 21:01:51 +0100

    hmm it's going to be more easier

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
                 INNER JOIN states ON cities.state_id = states.id\
                 ORDER BY cities.id")
-    city_list = cur.fetchall()
+    woof = cur.fetchall()
     for woof in row:
         print(woof)
     cur.close()
