53c02812c0a1f07ec49cdce066bb8c3d63f508b0
fadikoubaa19 <fadi.koubaa@gmail.com>
2020-12-13 22:16:58 +0100

    pls work

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module that contains city fetech"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}\
+@localhost/{}'.format(sys.argv[1],
+                      sys.argv[2],
+                      sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = sessionmaker()
+    session = session(bind=engine)
+    for i, j in session.query(City, State).filter(City.state_id.like(
+            State.id)):
+        print("{}: ({}) {}".format(j.name, i.id, i.name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module that contains model city"""
+
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import State
+
+Base = declarative_base()
+
+
+class City(Base):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True, unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
