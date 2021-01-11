818d860b5ab3287cd68d83b3dfca7c9a293f8356
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-01-11 20:04:12 +0100

    POST request to the passed URL with the email as a parameter

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 ''' POST request that pass an  URL and displays response'''
-import urllib.request as request
-import urllib.parse as parse
-from sys import argv
+
+import sys
+import urllib.parse
+import urllib.request
 
 if __name__ == "__main__":
-    url = argv[1]
-    data = {'email' : argv[2]}
-    data = parse.urlencode(data)
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(value)
     data = data.encode('ascii')
-    req = request.Request(url, data)
-    with request.urlopen(req) as res:
-        print(res.read().decode('utf-8'))
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
+
