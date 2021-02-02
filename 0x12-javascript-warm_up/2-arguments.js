03fcaa796db5b455b9bb35627a511c892f958b66
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-02-02 15:48:30 +0100

    if else

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let args = process.argv.length - 2;
+if (args < 1) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
