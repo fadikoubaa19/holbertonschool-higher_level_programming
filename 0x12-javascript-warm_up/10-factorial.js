9777f0e67d69f96df920a2e70881fbdee598e2dc
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-02-02 19:52:03 +0100

    fact task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const process = require('process');
-function factorial (i) {
-  if (i === 0) {
+function factorial (x) {
+  if (x === 0) {
     return (1);
   } else {
-    return (i * factorial(i - 1));
+    return (x * factorial(x - 1));
   }
 }
 console.log(factorial(parseInt(process.argv[2], 10) || 0));
