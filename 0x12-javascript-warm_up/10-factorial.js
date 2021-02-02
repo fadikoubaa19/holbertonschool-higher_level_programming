1188503348e91072e6f732efc66d773f98a687be
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-02-02 20:12:08 +0100

    fixxed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const process = require('process')
+const process = require('process');
 function factorial (x) {
   if (x === 0) {
-    return (1)
+    return (1);
   } else {
-    return (x * factorial(x - 1))
+    return (x * factorial(x - 1));
   }
 }
-console.log(factorial(parseInt(process.argv[2], 10) || 0))
+console.log(factorial(parseInt(process.argv[2], 10) || 0));
