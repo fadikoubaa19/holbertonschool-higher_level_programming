2ffd7ada15f075bad002271bbcf1590a882534f3
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-02-02 19:28:31 +0100

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let str in array) {
-  console.log(array[str]);
+const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let str in arr) {
+  console.log(arr[str]);
 }
