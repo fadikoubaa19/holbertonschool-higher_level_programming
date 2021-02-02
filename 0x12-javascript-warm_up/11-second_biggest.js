ee4a6efdcd65d6b2177d5d3394803cd2090a4735
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-02-02 20:14:46 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-const process = require('process');
-let largest = 0;
-let l = 0;
-for (let i = 2; parseInt(process.argv[i]); i++) {
-  if (parseInt(process.argv[i]) > largest){
-      largest=process.argv[i];
-}}
-console.log(largest)
+let argList = process.argv;
+if (argList.length <= 3) {
+  console.log(0);
+} else {
+  console.log(argList.sort().reverse()[1]);
+}
