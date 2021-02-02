ea6b3fcb1f855ccdc864fb58b54727fae2b56a72
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-02-02 19:39:41 +0100

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-let xxx = process.argv[2];
-if (isNaN(xxx)) {
+let square = process.argv[2];
+if (isNaN(square)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < xxx; i++) {
-    console.log('X'.repeat(xxx));
+  for (let i = 0; i < square; i++) {
+    console.log('X'.repeat(square));
   }
 }
