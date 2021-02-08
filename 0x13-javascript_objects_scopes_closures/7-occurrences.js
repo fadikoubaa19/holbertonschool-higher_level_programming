8e216bf3d6d5408a4d89dcb3538a518d1415694c
fadikoubaa19 <2062@holbertonschool.com>
2021-02-08 22:06:21 +0100

    occurance

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let total = 0;
+  for (let i in list) {
+    if (list[i] === searchElement) {
+      total++;
+    }
+  }
+  return count;
+};
