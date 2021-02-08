af57bc586bbe3111f66184fe34372591a94d65a6
fadikoubaa19 <2062@holbertonschool.com>
2021-02-08 10:10:17 +0100

    main file n2

---
+++ 0x13-javascript_objects_scopes_closures/main/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/nodes
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  if (w < 0) || (w = 0) {
+class Rectangle {
+}
+}
+  if (h < 0 ) || (h = 0) {
+class Rectangle {
+}
+  }
+}
+module.exports = Rectangle;
