158bced420a057a8aefca083a568c7d36b1834a0
fadikoubaa19 <2062@holbertonschool.com>
2021-02-11 11:21:41 +0100

    GET

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-let request = require('request');
-let rl = process.argv[2];
+const request = require('request');
+const rl = process.argv[2];
 request(rl, function (err, response) {
   if (err) {
     console.log(err);
