eea918531f947328f670596d3a9cd6946328a8ac
fadikoubaa19 <2062@holbertonschool.com>
2021-02-11 11:27:10 +0100

    api starwars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
 const request = require('request');
-const rl = 'http://swapi.co/api/films/:id';
-const episodeNumber = process.argv[2];
-request(rl + episodeNumber, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    console.log(JSON.parse(body).title);
-  } else {
-    console.log('Wrong status code');
-  }
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
+  if (error) { throw error; }
+  const jsn = JSON.parse(body);
+  for (const key in jsn) { if (key === 'title') console.log(jsn[key]); }
 });
