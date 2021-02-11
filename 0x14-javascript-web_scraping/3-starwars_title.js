3110d39fbb25ee2c25196f8a607c30f25655d705
fadikoubaa19 <2062@holbertonschool.com>
2021-02-11 11:22:22 +0100

    star wars API

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const rl = 'http://swapi.co/api/films/';
+const episodeNumber = process.argv[2];
+request(rl + episodeNumber, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    console.log(JSON.parse(body).title);
+  } else {
+    console.log('Wrong status code');
+  }
+});
